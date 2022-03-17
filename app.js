de47e0ca26e77c3755dac96d77c448d213ac3e62
V <2583695112@qq.com>
2022-03-17 12:45:31 +0800

    跨域

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const Koa = require("koa2");
 const app = new Koa();
 const port = 9000;
+const cors = require("koa2-cors");
+
+// 配置跨域（一定要在路由之前）
+app.use(cors());
 
 const router = require("./router/index");
 /*
